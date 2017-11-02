5985af60c8e97fa41a7c4427c3aaccc7a55aefc3
Alexandre Bléron <alex.bleron@gmail.com>
2017-11-03 07:42:31 +0800

    update

--- punchcard-client/src/main.kt
+++ punchcard-client/src/main.kt
@@ -1,5 +1,8 @@
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import com.github.thomasnield.rxkotlinfx.bind
+import com.github.thomasnield.rxkotlinfx.observeOnFx
+import io.reactivex.Flowable
 import io.reactivex.Observable
 import org.zeromq.ZMQ
 import io.reactivex.rxkotlin.toObservable
@@ -10,6 +13,12 @@ import tornadofx.*
 import javafx.scene.layout.*
 import javafx.scene.control.*
 import javafx.application.*
+import kotlinx.coroutines.experimental.Job
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.newSingleThreadContext
+import kotlinx.coroutines.experimental.reactive.consumeEach
+import kotlinx.coroutines.experimental.reactive.openSubscription
+import kotlinx.coroutines.experimental.reactive.publish
 
 val ENDPOINT = "tcp://127.0.0.1:1234"
 
@@ -23,6 +32,9 @@ class MainController: Controller() {
     val context : ZMQ.Context
     val socket: ZMQ.Socket
     val mapper = jacksonObjectMapper()
+    val listenJobs = ArrayList<Job>()
+    // We want to run ZMQ recv one only one thread
+    val coroutineContext = newSingleThreadContext("ListenerContext")
 
     init {
         context = ZMQ.context(1)
@@ -32,13 +44,26 @@ class MainController: Controller() {
         println("Connected")
     }
 
-    inline fun <reified T: Any> queryVariable(name: String): T {
-        socket.send(name)
-        val json = socket.recvStr()
-        //println("Received json: $json")
-        val v = mapper.readValue<T>(json)
-        println("Received value: $v")
-        return v
+    inline fun <reified T: Any> watchCoroutine(id: String) = publish<T>(coroutineContext) {
+        while (true) {
+            delay(1000)
+            socket.send(id)
+            val json = socket.recvStr() // this may block, but not the UI thread
+            val v = mapper.readValue<T>(json)
+            println("Received value: $v")
+            send(v)
+        }
+    }
+
+    // This is executed by the UI thread
+    inline fun <reified T: Any> remoteObservable(id: String) =
+        Flowable.fromPublisher(watchCoroutine<T>(id)).observeOnFx()
+
+    inline fun <reified T: Any> remoteWatch(id: String, crossinline action: (T) -> Unit) {
+        // Create a flowable, then observe it on the UI thread
+        remoteObservable<T>(id).subscribe {
+            action(it)
+        }
     }
 
     fun shutdown() {
@@ -55,8 +80,16 @@ class MyView : View() {
 
     init {
         with(root) {
-            textarea {
-                text = controller.queryVariable<Entity>("entity:0").toString()
+            vbox {
+                textfield() {
+                    textProperty().bind(controller.remoteObservable<Entity>("entity:0").map { it.toString() })
+                }
+                textfield() {
+                    textProperty().bind(controller.remoteObservable<Entity>("entity:1").map { it.toString() })
+                }
+                textfield() {
+                    textProperty().bind(controller.remoteObservable<Entity>("entity:2").map { it.toString() })
+                }
             }
         }
     }
@@ -64,17 +97,6 @@ class MyView : View() {
 
 class MyApp : App(MyView::class)
 
-
 fun main(args: Array<String>) {
     Application.launch(MyApp::class.java, *args)
-
-
-    // send a request for a specific entity
-
-    // receive some entities
-   /* for (i in 0..1000) {
-    } */
-
-    // disconnect
-    //
 }
